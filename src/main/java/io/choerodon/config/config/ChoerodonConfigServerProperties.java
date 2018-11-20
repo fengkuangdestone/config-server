7e456efa8c42aa457532ee6ff66d174e88a36ac0
fengkuangdestone <2668585@qq.com>
2018-11-20 15:35:28 +0800

    update md all

---
+++ src/main/java/io/choerodon/config/config/ChoerodonConfigServerProperties.java
@@ -0,0 +1,21 @@
+package io.choerodon.config.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author flyleft
+ */
+@ConfigurationProperties(prefix = "choerodon.config.server")
+public class ChoerodonConfigServerProperties {
+
+    private String managerService = "manager-service";
+
+    public String getManagerService() {
+        return managerService;
+    }
+
+    public void setManagerService(String managerService) {
+        this.managerService = managerService;
+    }
+
+}
